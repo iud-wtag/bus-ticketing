cafc0f088834f0485fbb4c3f25dae60a1aa42c42
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-03-13 19:12:17 +0600

    added: user model

---
+++ db/migrate/20230313114706_add_column_to_users.rb
@@ -0,0 +1,6 @@
+class AddColumnToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :user_name, :string
+    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
+  end
+end
