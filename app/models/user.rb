a7cddd62f721eeb5aa6878ee251ef3d15e118bc9
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-03-14 10:26:04 +0600

    added: model validation for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, :email, :password, :phone, :user_name, :role, presence: true
+  validates :phone, length: {is: 11}, numericality: { only_integer: true }
+  validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
+  validates :email, :user_name, uniqueness: {message: 'has already been taken'}
+  enum role: [:user, :admin]
 end
