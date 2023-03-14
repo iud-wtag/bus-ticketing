fd030a6cdcf61a7937df1490d519f00548f44e54
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-03-14 19:28:14 +0600

    added: payment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_14_103554) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_14_132618) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_14_103554) do
     t.string "bus_type", default: "", null: false
     t.string "bus_brand", default: "", null: false
     t.integer "bus_capacity", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "payments", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
