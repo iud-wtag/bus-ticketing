d7fac7465c7887c89f3730a3b3b43875cd53fb95
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-03-21 17:28:37 +0600

    added: CRUD of route model

--- app/controllers/routes_controller.rb
+++ app/controllers/routes_controller.rb
@@ -1,5 +1,59 @@
 class RoutesController < ApplicationController
   def index
-    @routes = Route.all
+    @routes = Route.all.order("id")
   end
+
+  def show
+    @route = Route.find(params[:id])
+  end  
+
+  def new
+    @route = Route.new
+  end
+  
+  def create
+    @route = Route.new(route_params)
+    if @route.save
+      flash[:success] = "Route was successfully Created"
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @route = Route.find(params[:id])
+  end
+
+  def update
+    @route = Route.find(params[:id])
+      if @route.update(route_params)
+        flash[:success] = "Route was successfully updated"
+        redirect_to @route
+      else
+        render :edit, status: :unprocessable_entity
+      end
+  end
+
+  def destroy
+    @route = Route.find(params[:id])
+    if @route.destroy
+      flash[:success] = 'Route was successfully deleted.'
+      redirect_to root_path, status: :see_other
+    else
+      flash[:error] = 'Something went wrong'
+      redirect_to routes_url
+    end
+  end
+  
+  
+
+  
+  
+
+  private
+    def route_params
+      params.require(:route).permit(:route_name, :route_destination, :route_source)
+    end 
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # root "routes#index"
 
-  # Defines the root path route ("/")
-  # root "articles#index"
-
-  root "routes#index"
-  get "/routes", to: "routes#index"
+  resources :routes
 end
