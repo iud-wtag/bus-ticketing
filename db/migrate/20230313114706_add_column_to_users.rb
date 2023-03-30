5f1f58800fa505a1ea5a29db4697f8edb7c82094
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-03-30 15:38:05 +0600

    removed: extra line

--- db/migrate/20230313114706_add_column_to_users.rb
+++ db/migrate/20230313114706_add_column_to_users.rb
@@ -1,6 +1,5 @@
 class AddColumnToUsers < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :user_name, :string
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end
--- db/migrate/20230315075446_add_role_column.rb
+++ db/migrate/20230315075446_add_role_column.rb
@@ -1,6 +1,5 @@
 class AddRoleColumn < ActiveRecord::Migration[7.0]
   def change
     add_column :users, :role, :integer, default: 0, null: false
-    #Ex:- add_column("admin_users", "username", :string, :limit =>25, :after => "email")
   end
 end
