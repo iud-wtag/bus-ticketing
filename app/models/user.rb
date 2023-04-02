5ad572d362b50f91c6dcebd59c9a12729c2a10d3
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-02 13:43:03 +0600

    added: phone validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   validates :name, :email, :password, :phone, :user_name, :role, presence: true
-  validates :phone, length: {minimum: 11, maximum: 14}
+  validates :phone, length: {minimum: 11, maximum: 15}, numericality: { only_integer: true }
   validates :email, format: {with: URI::MailTo::EMAIL_REGEXP}
   validates :email, :user_name, uniqueness: {message: 'has already been taken'}
   enum role: [:user, :admin]
