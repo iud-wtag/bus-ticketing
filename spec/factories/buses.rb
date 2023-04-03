462b02ff87229d8023328d68ea0d2d7c38627044
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-03 15:35:17 +0600

    added: spec folder & factory for Bus model

---
+++ spec/factories/buses.rb
@@ -0,0 +1,9 @@
+require "faker"
+FactoryBot.define do
+  factory :bus do
+    bus_name {Faker::Vehicle.license_plate}
+    bus_type {["AC","Non-AC"].sample}
+    bus_brand {Faker::Vehicle.manufacture}
+    bus_capacity {rand(1..100)}
+  end
+end
