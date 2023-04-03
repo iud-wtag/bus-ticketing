cedbc0d43aaca2d9d72e886c46cef18c63e5e31c
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-03 20:06:44 +0600

    added: migration for seats

--- db/migrate/20230403140121_create_seats.rb
+++ db/migrate/20230403140121_create_seats.rb
@@ -4,7 +4,6 @@ class CreateSeats < ActiveRecord::Migration[7.0]
       t.string :seat_name, default: "", null: false
       t.boolean :seat_booked, default: false, null: false
       t.references :bus, null: false, foreign_key: true
-
       t.timestamps
     end
   end
