0f6a61056618d37d77431aa5e508e4ab897559e8
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-06 16:17:03 +0600

    update: line format

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -1,10 +1,7 @@
 class Bus < ApplicationRecord
   validates :name, :typed, :brand, :capacity, presence:true
   validates :capacity, numericality: { only_integer: true, less_than_or_equal_to: 50 }
-  validates :name, uniqueness: { 
-    message: 'has already been taken' },  
-    format: { without: /\s/, message: "cannot contain spaces" 
-  }
+  validates :name, uniqueness: { message: 'has already been taken' }, format: { without: /\s/, message: "cannot contain spaces" }
   validates :brand, format: { without: /\d/, message: "cannot contain digits" }
 
   enum typed: { AC: 0, NON_AC: 1 }
