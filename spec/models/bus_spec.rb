b3b0ba57b17593d5893653a5bf18fb04c4c428ef
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-06 15:42:34 +0600

    updated: test for valid and invalid value

--- spec/models/bus_spec.rb
+++ spec/models/bus_spec.rb
@@ -11,6 +11,12 @@ RSpec.describe Bus do
     it { should validate_numericality_of(:capacity).only_integer.is_less_than_or_equal_to(50) }
     it { should validate_uniqueness_of(:name).with_message("has already been taken") }
 
+    it { should allow_value("letterOnly").for(:brand) }
+    it { should allow_value("nospace-123").for(:name) }
+
+    it { should_not allow_value("123somevalue").for(:brand) } 
+    it { should_not allow_value("space text").for(:name) } 
+
     it "has a valid factory" do
       expect(bus).to be_valid
     end
