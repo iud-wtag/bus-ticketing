cd5fbb83f2a824af6404ae5421f16718634c0425
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-06 12:53:59 +0600

    added: test for ticket

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:user) { create(:user) }
+  let(:bus) { create(:bus) }
+  let(:route) { create(:route) }
+  let(:trip) { create(:trip, bus: bus, route: route) }
+  let(:payment) { create(:payment) }
+  let(:ticket) { build(:ticket, user: user, bus: bus, trip: trip, payment: payment) }
+
+  describe "associations" do
+    it { should belong_to(:user) }
+    it { should belong_to(:bus) }
+    it { should belong_to(:trip) }
+    it { should belong_to(:payment) }
+    it { should have_many(:seats).dependent(:destroy) }
+  end
+  
+  describe 'validations' do
+    it { should validate_presence_of(:total_fare) }
+  end
 end
