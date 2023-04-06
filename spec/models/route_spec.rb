c985accc32dca9a5f459447689c0192789b916ca
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-06 16:49:16 +0600

    reordered

--- spec/models/route_spec.rb
+++ spec/models/route_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Route do
+  let!(:route) { FactoryBot.create(:route) }
   describe "validations" do
-    let!(:route) { FactoryBot.create(:route) }
 
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:source) }
