d4aaca9524df0645a393a85bc4590e98fee8bea5
Imtiaj Rafi <imtiaj.uddin@welldev.io>
2023-04-13 19:35:07 +0600

    added: seat and trip booking for ticket controller

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,5 +1,54 @@
 class TicketsController < ApplicationController
+  before_action :depend_params, only: %i[select_trip]
+
   def index
     @tickets = Ticket.all.order("id")
   end
+
+  def select_trip
+    @sources = Route.select('DISTINCT ON (source) *')
+    @destinations = @destination || []
+    @trips = @trip || []
+  end
+
+  def select_seat
+    @trip = Trip.find_by(id: params[:id])
+    @bus = Bus.find_by(id: @trip.bus.id)
+    @seats = @bus.seats.order(id: :asc)
+  end  
+
+  def confirm_order
+    
+  end  
+
+  def payment
+    @seats = []
+    params.each do |key,value|
+      @seats.push(value) if key == value
+    end  
+    if @seats.size==0
+      flash[:alert] = 'Select at least 1 seat'
+      redirect_to select_seat_path(params[:trip_id]), status: :see_other
+    else
+      @seat_session = session[:seats] || []
+      session[:seats] = @seats
+      session[:trip] = params[:trip_id]
+      redirect_to confirm_order, status: :see_other
+    end
+  end
+  
+  private
+    def ticket_params
+      params.require(:ticket).permit(:total_fare, :user_id, :bus_id, :trip_id, :payment_id)
+    end
+
+    def depend_params
+      @source = Route.find_by(id: params[:source].presence)
+      @destination = Route.where(source: params[:source].presence)
+      trip_date = DateTime.parse(params[:trip_datetime]).to_date
+      @trip = Trip.where(trip_datetime: trip_date.beginning_of_day..trip_date.end_of_day,
+                         route: Route.find_by(source: params[:source].presence,
+                                              destination: params[:destination].presence ))
+    end  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
 
   resources :users, :routes, :buses, :trips, :tickets
+
+  get 'select_trip', to: 'tickets#select_trip'
+  get 'select_seat/:id', to: 'tickets#select_seat', as: "select_seat"
+  post 'payment/23', to: 'tickets#payment', as: 'payment'
 end
